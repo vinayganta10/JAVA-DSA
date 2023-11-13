ebb1410cd4ddc8904883eeaf0a8dcd86e659ab73
vinay ganta <aggvinayganta10@gmail.com>
2023-11-13 07:14:47 +0530

    added some problems

---
+++ src/PhoneCombinations.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class PhoneCombinations {
+    public static List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) return Collections.emptyList();
+
+        String[] phone_map = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> output = new ArrayList<>();
+        backtrack("", digits, phone_map, output);
+        return output;
+    }
+
+    private static void backtrack(String combination, String next_digits, String[] phone_map, List<String> output) {
+        if (next_digits.isEmpty()) {
+            output.add(combination);
+        } else {
+            String letters = phone_map[next_digits.charAt(0) - '2'];
+            for (char letter : letters.toCharArray()) {
+                backtrack(combination + letter, next_digits.substring(1), phone_map, output);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(letterCombinations("23"));
+    }
+}
