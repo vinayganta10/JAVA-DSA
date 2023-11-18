8d77111a4515c15ea1e70d14696a63a26dcefa56
vinay ganta <aggvinayganta10@gmail.com>
2023-11-18 17:33:52 +0530

    added some problems

---
+++ src/SwapNodesInPairs.java
@@ -0,0 +1,29 @@
+public class SwapNodesInPairs {
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = dummy;
+
+        while (curr.next != null && curr.next.next != null) {
+            ListNode first = curr.next;
+            ListNode second = curr.next.next;
+            curr.next = second;
+            first.next = second.next;
+            second.next = first;
+            curr = curr.next.next;
+        }
+
+        return dummy.next;
+    }
+}
